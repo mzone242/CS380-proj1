c47353aeab819dd93c0f5494a1fd88d385121010
Ubuntu <ubuntu@master0.cluster.local>
2023-10-01 18:22:57 -0400

    better workflow

--- dockerfiles/base.dockerfile
+++ dockerfiles/base.dockerfile
@@ -7,9 +7,12 @@ USER root
 ARG DEBIAN_FRONTEND=noninteractive
 ENV TZ=Etc/UTC
 
-RUN apt-get update && apt-get install -y git
+COPY server.py /cs380d-f23/project1.server.py
+COPY frontend.py /cs380d-f23/project1/frontend.py
 
-RUN git clone https://github.com/mzone242/CS380-proj1.git
+# RUN apt-get update && apt-get install -y git
+
+# RUN git clone https://github.com/mzone242/CS380-proj1.git
 
 ENV KVS_HOME /CS380-proj1
 
--- scripts/clean_build_docker.sh
+++ scripts/clean_build_docker.sh
@@ -2,18 +2,14 @@
 
 sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' | grep 'mzone242' | awk '{print $2}')
 
-cd dockerfiles
-
-sudo docker build --network=host . -f base.dockerfile -t mzone242/raft:base
+sudo docker build --network=host . -f dockerfiles/base.dockerfile -t mzone242/raft:base
 sudo docker push mzone242/raft:base
 
-sudo docker build . -f client.dockerfile -t mzone242/raft:client
+sudo docker build . -f dockerfiles/client.dockerfile -t mzone242/raft:client
 sudo docker push mzone242/raft:client
 
-sudo docker build . -f frontend.dockerfile -t mzone242/raft:frontend
+sudo docker build . -f dockerfiles/frontend.dockerfile -t mzone242/raft:frontend
 sudo docker push mzone242/raft:frontend
 
-sudo docker build . -f server.dockerfile -t mzone242/raft:server
+sudo docker build . -f dockerfiles/server.dockerfile -t mzone242/raft:server
 sudo docker push mzone242/raft:server
-
-cd ..
